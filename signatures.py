bec6f66d44cdcef532ea8d555378d3c9a192fb18
Seweryn Załupka <s.zalupka@nomtek.com>
2019-03-07 11:12:40 +0100

    added signatures.py for handling signatures dataset

---
+++ signatures.py
@@ -0,0 +1,15 @@
+from dataset import load_cached
+import download
+import os
+
+data_dir = "data/sigComp2011/"
+train_dir = os.path.join(data_dir, "train/")
+test_dir = os.path.join(data_dir, "test/")
+
+train_data_url = "http://www.iapr-tc11.org/dataset/ICDAR_SignatureVerification/SigComp2011/sigComp2011-trainingSet.zip"
+test_data_url = "http://www.iapr-tc11.org/dataset/ICDAR_SignatureVerification/SigComp2011/sigComp2011-test.zip"
+
+
+def maybe_download_and_extract():
+    download.maybe_download_and_extract(url=train_data_url, download_dir=data_dir)
+    download.maybe_download_and_extract(url=test_data_url, download_dir=data_dir)
